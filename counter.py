bd1d72bbc492e631ce86c2a420655c1cc3a5123f
haibui01 <48041896+haibui01@users.noreply.github.com>
2025-09-18 20:00:13 +0700

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,39 +1,25 @@
 import flet as ft
 
-
-def example(page):
-    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
+def main(page: ft.Page):
+    txt_number = ft.TextField(value="0", text_align="right", width=100)
 
     def minus_click(e):
         txt_number.value = str(int(txt_number.value) - 1)
-        e.control.page.update()
+        page.update()
 
     def plus_click(e):
         txt_number.value = str(int(txt_number.value) + 1)
-        e.control.page.update()
+        page.update()
 
-    return ft.Column(
-        expand=True,
-        alignment=ft.MainAxisAlignment.CENTER,
-        controls=[
-            ft.Row(
-                [
-                    ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
-                    txt_number,
-                    ft.IconButton(ft.icons.ADD, on_click=plus_click),
-                ],
-                alignment=ft.MainAxisAlignment.CENTER,
-            )
-        ],
+    page.add(
+        ft.Row(
+            [
+                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
+                txt_number,
+                ft.IconButton(ft.icons.ADD, on_click=plus_click),
+            ],
+            alignment="center",
+        )
     )
 
-
-def main(page: ft.Page):
-    page.title = "Flet counter example"
-    page.window_width = 390
-    page.window_height = 844
-    page.add(example(page))
-
-
-if __name__ == "__main__":
-    ft.app(target=main)
+ft.app(target=main)
